55eac380c89babe034408d994af5d8db52e88332
Сырцев Илья <94793965+Zevs41@users.noreply.github.com>
2022-11-21 21:54:51 +0300

    Add comment

--- php/add_record.php
+++ php/add_record.php
@@ -1,3 +1,4 @@
+<---Подключение к БД--->
 <?php
 
 $dbHost = "localhost";
@@ -9,6 +10,7 @@ if ($mysqli == false) {
     echo ("Ошибка подключения к БД");
 }
 
+<---Валидация заголовка--->
 $title = $_POST['title'];
 
 if(mb_strlen($title) < 4 || mb_strlen($title)>50){
@@ -16,6 +18,7 @@ if(mb_strlen($title) < 4 || mb_strlen($title)>50){
     exit();
 }
 
+<---Валидация текста--->
 $text = $_POST['text'];
 
 if(mb_strlen($text) < 500 || mb_strlen($text)>5000){
@@ -23,6 +26,7 @@ if(mb_strlen($text) < 500 || mb_strlen($text)>5000){
     exit();
 }
 
+<---Добавление записи в БД--->
 mysqli_query($mysqli, "INSERT INTO `records` (`title`, `text`, `author`) VALUES ('$title', '$text', '$_COOKIE[user]')");
 
 $mysqli -> close();
